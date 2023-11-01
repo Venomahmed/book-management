cd1ed61e5d0ef24e1a966a05b643983a4a5c15ab
istiakahmed <ahmed.istiak0318@gmail.com>
2023-11-01 12:30:53 -0400

    added java docs

--- src/main/java/com/ness/bookmanagement/service/author/AuthorService.java
+++ src/main/java/com/ness/bookmanagement/service/author/AuthorService.java
@@ -6,42 +6,54 @@ import com.ness.bookmanagement.dto.BookDTO;
 import java.util.List;
 
 /**
- *
+ * Service interface for managing authors.
  */
 public interface AuthorService {
 
     /**
-     * @param authorDTO
-     * @return
+     * Create a new author.
+     *
+     * @param authorDTO The AuthorDTO object representing the author to be created.
+     * @return The created AuthorDTO.
      */
     AuthorDTO createAuthor(AuthorDTO authorDTO);
 
     /**
-     * @return
+     * Retrieve a list of all authors.
+     *
+     * @return A list of AuthorDTO objects representing all authors.
      */
     List<AuthorDTO> getAllAuthors();
 
     /**
-     * @param id
-     * @return
+     * Retrieve an author by their ID.
+     *
+     * @param id The ID of the author to retrieve.
+     * @return The AuthorDTO object representing the retrieved author.
      */
     AuthorDTO getAuthorById(Long id);
 
     /**
-     * @param id
-     * @param updatedAuthorDTO
-     * @return
+     * Update an author's information.
+     *
+     * @param id               The ID of the author to be updated.
+     * @param updatedAuthorDTO The AuthorDTO object containing updated information.
+     * @return The updated AuthorDTO.
      */
     AuthorDTO updateAuthor(Long id, AuthorDTO updatedAuthorDTO);
 
     /**
-     * @param id
+     * Delete an author by their ID.
+     *
+     * @param id The ID of the author to be deleted.
      */
     void deleteAuthor(Long id);
 
     /**
-     * @param authorId
-     * @return
+     * Retrieve a list of books written by a specific author.
+     *
+     * @param authorId The ID of the author for whom to retrieve books.
+     * @return A list of BookDTO objects representing the books written by the author.
      */
     List<BookDTO> getBooksByAuthor(Long authorId);
 }
--- src/main/java/com/ness/bookmanagement/service/book/BookService.java
+++ src/main/java/com/ness/bookmanagement/service/book/BookService.java
@@ -6,48 +6,62 @@ import com.ness.bookmanagement.dto.BookFilterDTO;
 import java.util.List;
 
 /**
- *
+ * Service interface for managing books.
  */
 public interface BookService {
 
     /**
-     * @param bookDTO
-     * @return
+     * Creates a new book.
+     *
+     * @param bookDTO The book data to be created.
+     * @return The created book DTO.
      */
     BookDTO createBook(BookDTO bookDTO);
 
     /**
-     * @return
+     * Retrieves all books.
+     *
+     * @return A list of all book DTOs.
      */
     List<BookDTO> getAllBooks();
 
     /**
-     * @param id
-     * @return
+     * Retrieves a book by its unique identifier.
+     *
+     * @param id The ID of the book to retrieve.
+     * @return The book DTO with the specified ID.
      */
     BookDTO getBook(Long id);
 
     /**
-     * @param isbn
-     * @return
+     * Retrieves a book by its ISBN (International Standard Book Number).
+     *
+     * @param isbn The ISBN of the book to retrieve.
+     * @return The book DTO with the specified ISBN.
      */
     BookDTO getBookByIsbn(String isbn);
 
     /**
-     * @param id
-     * @param updatedBookDTO
-     * @return
+     * Updates an existing book with new data.
+     *
+     * @param id             The ID of the book to update.
+     * @param updatedBookDTO The updated book data.
+     * @return The updated book DTO.
      */
     BookDTO updateBook(Long id, BookDTO updatedBookDTO);
 
     /**
-     * @param id
+     * Deletes a book by its unique identifier.
+     *
+     * @param id The ID of the book to delete.
      */
     void deleteBook(Long id);
 
     /**
-     * @param bookFilterDTO
-     * @return
+     * Filters books based on the specified criteria in the provided filter DTO.
+     *
+     * @param bookFilterDTO The filter criteria to apply.
+     * @return A list of book DTOs that match the filter criteria.
      */
     List<BookDTO> filterBooks(BookFilterDTO bookFilterDTO);
 }
